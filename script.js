19cf8651a0c60af2d4aa40123ca87dcc38a903e1
Dhruv Sharma <134517753+testgithubrittttttt@users.noreply.github.com>
2024-05-05 02:07:52 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+window.onload = function () {
+	setInterval(updateTime, 1000)
+};
+
+function updateTime() {
+	const format = number => `${ +!(number < 10) && '' }${number}`;
+	
+	let date = new Date();
+	
+	document.querySelector("#hour")  .innerHTML = format( date.getHours()   );
+	document.querySelector("#minute").innerHTML = format( date.getMinutes() );
+	document.querySelector("#second").innerHTML = format( date.getSeconds() );
+
+	document.querySelector("#date").innerHTML = (
+		[
+			date.getDate(),
+      ++date.getMonth(), 
+			date.getFullYear()
+		]
+			.map(format)
+			.join('/')
+	);
+	
+	const week = [
+		"Sunday", "Monday", "Tuesday", "Wednesday",
+        "Thursday", "Friday", "Saturday"
+	];
+	
+	document.querySelector("#day").innerHTML = week[ date.getDay() ];
+}
